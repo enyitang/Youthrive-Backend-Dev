767c39db3c8e483a6222ac81aed1f7d710c48f32
Joseph Enyitang <enyitang56@gmail.com>
2024-04-08 17:03:28 +0100

    Add files via upload
    7-4-2023
    Assignment(Classwork)

---
+++ Datatype.js
@@ -0,0 +1,26 @@
+
+//String datatype
+
+let MyNameAndSurnameOutput = "Joseph Enyitang"
+console.log(MyNameAndSurnameOutput)
+
+
+//Number Datatype
+
+let MyFavouriteNumberForTheYear=26
+console.log(MyFavouriteNumberForTheYear)
+
+
+//boolean Datatype
+let MyBooleanBringPositiveResult = true
+console.log(MyBooleanBringPositiveResult)
+
+
+//Null Datatype
+let MyNullTestAndSee = null
+console.log(MyNullTestAndSee)
+
+
+//Undefined Datatype
+let MyCodeRunVeryWell;
+console.log(MyCodeRunVeryWell)
